62b6ac453714297e023f8b76f08cb2a779ae2e44
Barion <kothe@botos.de>
2023-06-26 18:05:28 +0200

    made package

--- Assets/Ametrin/Console/Command/IComandArgumentParser.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace Ametrin.Console.Command{
-    public interface ICommandArgumentParser{
-        public object Parse(string raw);
-
-        public string[] GetSuggestions() => Array.Empty<string>();
-    }
-}
--- Assets/Test/Item.cs
+++
@@ -1,7 +0,0 @@
-using Ametrin.AutoRegistry;
-using UnityEngine;
-
-[CreateAssetMenu]
-public sealed class Item : ScriptableObject{
-    public static readonly ScriptableObjectRegistry<string, Item> Registry = new(item => item.name);
-}
